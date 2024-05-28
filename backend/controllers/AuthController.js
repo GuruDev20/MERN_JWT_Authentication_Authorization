edeb83a72a22c2e1803298134cf2f894c044c7ef
Dev <guru01803@gmail.com>
2024-05-28 14:30:05 +0530

    added logout function

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -42,4 +42,14 @@ const login=async(req,res)=>{
     }
 }
 
-module.exports={register,login};
+const logout=async(req,res)=>{
+    try{
+        res.clearCookie('token')
+        res.status(200).json({message:"Logout successfully"});
+    }
+    catch(error){
+        res.status(500).json({success:false,message:"Internal server error"});
+    }
+}
+
+module.exports={register,login,logout};
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -3,5 +3,6 @@ const auth=require('../controllers/AuthController');
 const AuthRoutes=express.Router();
 AuthRoutes.post('/register',auth.register);
 AuthRoutes.post('/login',auth.login);
+AuthRoutes.post('/logout',auth.logout);
 
 module.exports=AuthRoutes;
